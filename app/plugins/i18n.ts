06aac532ace505bb8bdf6ea0ed82d020cfb2acf1
Skyost <me@skyost.eu>
2026-05-05 13:23:20 +0200

    feat: Localized the website into `de`, `it` and `pt`.

--- app/plugins/i18n.ts
+++ app/plugins/i18n.ts
@@ -1,6 +1,9 @@
 import { createI18n } from 'vue-i18n'
+import de from '~/locales/de.json'
 import en from '~/locales/en.json'
 import fr from '~/locales/fr.json'
+import it from '~/locales/it.json'
+import pt from '~/locales/pt.json'
 
 export default defineNuxtPlugin(({ vueApp }) => {
   const i18n = createI18n({
@@ -10,8 +13,11 @@ export default defineNuxtPlugin(({ vueApp }) => {
     fallbackLocale: 'en',
     ssr: true,
     messages: {
+      de,
       en,
       fr,
+      it,
+      pt,
     },
   })
 
